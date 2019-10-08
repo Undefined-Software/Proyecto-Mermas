f83854b2f61e515a7994cf346797d202727e591f
Isaac <isaac1998188@gmail.com>
2019-10-08 11:04:31 -0700

    Se crean componentes navbar con plantilla vacía, productos y mermas

--- Mermas-Frontend/src/app/app.module.ts
+++ Mermas-Frontend/src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductosComponent } from './components/productos/productos.component';
+import { MermasComponent } from './components/mermas/mermas.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent,
+    MermasComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Mermas-Frontend/src/app/components/mermas/mermas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mermas',
+  templateUrl: './mermas.component.html',
+  styleUrls: ['./mermas.component.css']
+})
+export class MermasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
