4a3c7ea2610beeb7fc8582696c3b628d8ef78326
Isaac <isaac1998188@gmail.com>
2019-10-11 10:09:47 -0700

    Se cambia la base de datos a sql lite en el settings.py por problemas al querer ejecutar el server

--- Mermas-Backend/fatboy/settings.py
+++ Mermas-Backend/fatboy/settings.py
@@ -98,14 +98,21 @@ WSGI_APPLICATION = 'fatboy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'fatboybd', #Nombre de la bd
+#        'USER': 'root',     #Usuario
+#        'PASSWORD': '',     #Contraseña
+#        'HOST': '127.0.0.1',
+#        'PORT': '3306',     #Puerto
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'fatboybd', #Nombre de la bd
-        'USER': 'root',     #Usuario
-        'PASSWORD': '',     #Contraseña
-        'HOST': '127.0.0.1',
-        'PORT': '3306',     #Puerto
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
