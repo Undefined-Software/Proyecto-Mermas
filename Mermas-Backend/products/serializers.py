4c06e5818972e082c9439c267827e22b57065075
Alfredo <alfredo.rvlpz@gmail.com>
2019-10-11 10:28:36 -0700

    Filtro
    Filtro para obtener el producto con la primera coincidencia

--- Mermas-Backend/products/serializers.py
+++ Mermas-Backend/products/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from products.models import Product
 
+"""class ProductListSerializer(serializers.ListSerializer):
+    def create(self, validated_data):
+        print("aH VEDA")
+        #books = [Product(**item) for item in validated_data]
+        books=[ProductSerializer(item)for item in validated_data]
+        return Product.objects.bulk_create(books)"""
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id','name','price','code','image', 'amount')
+        fields = ('__all__')
+        #list_serializer_class = ProductListSerializer
--- Mermas-Backend/products/views.py
+++ Mermas-Backend/products/views.py
@@ -1,19 +1,20 @@
 from rest_framework import generics
 from rest_framework.response import Response
-
-from products.serializers import ProductSerializer
+from products.serializers import ProductSerializer #, ProductListSerializer
 from .models import Product
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 
 class ProductList(generics.ListCreateAPIView): #permite el listado y creación 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-class ProductDetail(generics.RetrieveUpdateDestroyAPIView): #Permite la actualización y destrucción de un elemento por 1 id
+#Permite la actualización y destrucción de un elemento por 1 id 
+#formato path/id
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView): 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer 
 
-class MultipleFieldLookupMixin(object):
+"""class MultipleFieldLookupMixin(object): 
     def get_object(self):
         queryset = self.get_queryset()
         queryset = self.filter_queryset(queryset)
@@ -28,4 +29,43 @@ class MultipleFieldLookupMixin(object):
 class RetrieveProductView(MultipleFieldLookupMixin, generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    lookup_fields = ['name', 'amount']
+    lookup_fields = ['name', 'amount']"""
+
+class FieldLookupMixin(object):
+    def get_object(self):
+        queryset = self.get_queryset()
+        queryset = self.filter_queryset(queryset)
+        filter = {}
+        obj=[]
+        filter[self.kwargs['name']]=self.kwargs['value']
+        obj+=get_list_or_404(queryset, **filter)
+        print(obj)
+        self.check_object_permissions(self.request, obj)
+        return obj[0]
+
+
+#Permite la busqueda en base a cualquier campo el formato de entrada es "path/nombredelcampo/valordelcampo"
+#si el nombre del campo esta mal marca error 
+class FilterProductView(FieldLookupMixin,generics.RetrieveAPIView): 
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer #(child=ProductSerializer()).create(validated_data=queryset)#(queryset,many=True)
+    #print("-------------------------sali-------------------------")
+"""
+class MultiFieldLookupMixin(object):
+    def get_object(self):
+        queryset = self.get_queryset()
+        queryset = self.filter_queryset(queryset)
+        filter = {}
+        l=self.kwargs['field_list']
+        return l[1]
+        for i in range(0,len(self.kwargs),2):
+            if (self.kwargs['field_list'].[i]!=None and self.kwargs['field_list'].[i+1]!=None):
+                filter[self.kwargs.field_list[i]]=self.kwargs.field_list[i+1]
+        obj = get_object_or_404(queryset, **filter)
+        self.check_object_permissions(self.request, obj)
+        return obj   
+
+
+class MultiFilterProductView(MultiFieldLookupMixin,generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer"""
