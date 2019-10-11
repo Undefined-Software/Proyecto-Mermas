cbf7bfe08004c0af523d4035be9283f96539daa1
Alfredo <alfredo.rvlpz@gmail.com>
2019-10-11 04:30:36 -0700

    Recuparación por multifiltro
    Se recupera la información de la base de datos a manera de multifiltro, especificamente nombre y cantidad

--- Mermas-Backend/products/urls.py
+++ Mermas-Backend/products/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
     path('products/<int:pk>/', views.ProductDetail.as_view()),
+    path('products/ss/<str:name>/<str:amount>/', views.RetrieveProductView.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- Mermas-Backend/products/views.py
+++ Mermas-Backend/products/views.py
@@ -11,4 +11,21 @@ class ProductList(generics.ListCreateAPIView): #permite el listado y creación
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView): #Permite la actualización y destrucción de un elemento por 1 id
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer    
+    serializer_class = ProductSerializer 
+
+class MultipleFieldLookupMixin(object):
+    def get_object(self):
+        queryset = self.get_queryset()
+        queryset = self.filter_queryset(queryset)
+        filter = {}
+        for field in self.lookup_fields:
+            if self.kwargs[field]:
+                filter[field] = self.kwargs[field]
+        obj = get_object_or_404(queryset, **filter)
+        self.check_object_permissions(self.request, obj)
+        return obj   
+
+class RetrieveProductView(MultipleFieldLookupMixin, generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_fields = ['name', 'amount']
