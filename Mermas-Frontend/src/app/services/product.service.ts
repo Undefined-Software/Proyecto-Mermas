757e87365e3a654f098f590e40204a1d90632e24
Isaac <isaac1998188@gmail.com>
2019-10-11 10:32:58 -0700

    se agrega el metodo getProductos en el servicio y el componente de productos, con sus imports correspondientes

--- Mermas-Frontend/src/app/services/product.service.ts
+++ Mermas-Frontend/src/app/services/product.service.ts
@@ -1,9 +1,15 @@
 import {Injectable} from '@angular/core';
 import {HttpHeaders, HttpClient} from '@angular/common/http';
+import {Product} from '../models/product.model';
+import {Observable} from 'rxjs';
 
 @Injectable()
-export class productoService{
+export class productService{
     constructor(private http: HttpClient){}
     private headers = new HttpHeaders().set('Content-Type','application/json');  
     public url:string = 'http://localhost:8000';
+
+    getProductos(): Observable<any>{  
+        return this.http.get(this.url+'/api/products/', {headers: this.headers})     
+    }
 }
